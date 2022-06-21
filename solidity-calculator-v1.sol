fb36649fb6afb7e9bdf9f6646c64e644ba754f85
imthebiggestguy <107901643+imthebiggestguy@users.noreply.github.com>
2022-06-20 23:02:33 -0500

    Update solidity-calculator-v1.sol

--- solidity-calculator-v1.sol
+++ solidity-calculator-v1.sol
@@ -2,29 +2,27 @@ pragma solidity >=0.5.0 <0.6.0;
 
 contract Calculator{
 
-function add(uint a, uint b) public {
-  c = a + b
-}
+  function add(uint a, uint b) public {
+    c = a + b
+  }
 
-function subtract(uint a, uint b) public {
-  c = a - b
-}
+  function subtract(uint a, uint b) public {
+    c = a - b
+  }
 
-function multiply(uint a, uint b) public {
-  c = a * b
-}
+  function multiply(uint a, uint b) public {
+    c = a * b
+  }
 
-function divide(uint a, uint b) public {
-  c = a / b
-}
+  function divide(uint a, uint b) public {
+    c = a / b
+  }
 
-function exp(uint a, uint b) public {
-  c = a ** b
-}
+  function exp(uint a, uint b) public {
+    c = a ** b
+  }
 
-function Result() public view returns (uint c) {
-  return c;
-}
-
-
+  function Result() public view returns (uint c) {
+    return c;
+  }
 }
