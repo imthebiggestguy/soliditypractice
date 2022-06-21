1edb669150f557afadf9008eb0d3d09388052a25
imthebiggestguy <107901643+imthebiggestguy@users.noreply.github.com>
2022-06-20 23:08:52 -0500

    Update solidity-calculator-v1.sol

--- solidity-calculator-v1.sol
+++ solidity-calculator-v1.sol
@@ -1,25 +1,26 @@
 pragma solidity >=0.5.0 <0.6.0;
 
 contract Calculator{
+uint c;
 
   function add(uint a, uint b) public {
-    c = a + b
+    c = a + b;
   }
 
   function subtract(uint a, uint b) public {
-    c = a - b
+    c = a - b;
   }
 
   function multiply(uint a, uint b) public {
-    c = a * b
+    c = a * b;
   }
 
   function divide(uint a, uint b) public {
-    c = a / b
+    c = a / b;
   }
 
   function exp(uint a, uint b) public {
-    c = a ** b
+    c = a ** b;
   }
 
   function Result() public view returns (uint c) {
